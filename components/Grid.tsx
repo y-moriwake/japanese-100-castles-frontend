d156340b69dd558548a6aae8c0657cb893157222
y-moriwake <moriwake.yutaka@myutaka-engineer.com>
2021-01-16 00:11:03 +0900

    [refactor] インポート文少し修正

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import { CastleInfo } from '../interfaces/castleInfo';
+import { CastleInfo } from '../interfaces/CastleInfo';
 import SimpleCard from './Card';
 
 const useStyles = makeStyles((theme: Theme) =>
