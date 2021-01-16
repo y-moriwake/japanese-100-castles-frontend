7495dde786541ce4e17ab9a9180791b7595160de
y-moriwake <moriwake.yutaka@myutaka-engineer.com>
2021-01-16 16:42:35 +0900

    [add] tailwindcssの導入

--- components/Card.tsx
+++ components/Card.tsx
@@ -5,38 +5,36 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import CastleInfo from '../interfaces/CastleInfo';
 
 const useStyles = makeStyles({
-  root: {
-    margin:10,
-    backgroundColor: "#89c3eb",
-  },
-  title: {
-    fontSize: 15,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-});
+    root: {
+      margin:10,
+      backgroundColor: "#89c3eb",
+    },
+  });
 
-function SimpleCard({ regionalName, castlesName, id}: CastleInfo) {
+type Props = {
+    regionalName: string,
+    castlesName: string
+}
+
+function SimpleCard({ regionalName, castlesName }: Props) {
     const classes = useStyles();
 
     return (
-        <Card className={classes.root} key={id}>
-            <CardContent>
-                <Typography variant="h4" component="h2">
-                    {regionalName}
-                </Typography>
-                <Typography variant="body2" component="p">
-                    {castlesName}
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button size="small">Learn More</Button>
-            </CardActions>  
-        </Card>
+            <Card className={classes.root} >
+                <CardContent>
+                    <Typography variant="h4" component="h2">
+                        {regionalName}
+                    </Typography>
+                    <Typography variant="body2" component="p">
+                        {castlesName}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small">Learn More</Button>
+                </CardActions>
+            </Card>
     );
 }
 
