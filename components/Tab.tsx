932d0ab9d19f5842406df0829a815bec6f863e6d
y-moriwake <moriwake.yutaka@myutaka-engineer.com>
2021-01-20 23:19:11 +0900

    [add] 姫路城写真を追加

---
+++ components/Tab.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
+const useStyles = makeStyles({
+    root: {
+        flexGrow: 1,
+        backgroundColor: "#dddcd6"
+    },
+});
+
+const CenteredTabs = () => {
+    const classes = useStyles();
+    const [value, setValue] = React.useState(1);
+
+    const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+        setValue(newValue);
+        // 押されたタブのindex
+        console.log(`${value}`);
+    };
+
+    return (
+        <Paper className={classes.root}>
+            <Tabs
+                value={value}
+                onChange={handleChange}
+                indicatorColor="primary"
+                textColor="primary"
+                centered
+            >
+                <Tab label="日本100名城" />
+                <Tab label="Coming Soon..." />
+                <Tab label="Coming Soon..." />
+                <Tab label="利用規約" />
+            </Tabs>
+        </Paper>
+    );
+}
+
+export default CenteredTabs;
